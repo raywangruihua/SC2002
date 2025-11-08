c8027195a0862794d2b49d65f0b902de75af3a03
tenkuroh <165874907+tenkuroh@users.noreply.github.com>
2025-11-09 02:26:33 +0800

    Update InternshipApplication.java

--- src/InternshipApplication.java
+++ src/InternshipApplication.java
@@ -5,4 +5,13 @@ public class InternshipApplication {
 	private String internshipTitle;
 	private ApplicationStatus status;
 
+	public InternshipApplication(String studentName, String internshipTitle){
+		this.studentName = studentName;
+		this.internshipTitle = internshipTitle;
+		this.status = ApplicationStatus.Pending;
+	}
+
+	public String getInternshipTitle(){
+		return internshipTitle;
+	}
 }
