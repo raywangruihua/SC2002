012cef69bf45b37b771fcb33f65a0d3138c674ad
tenkuroh <165874907+tenkuroh@users.noreply.github.com>
2025-11-09 02:25:53 +0800

    Update Repository.java

--- src/Repository.java
+++ src/Repository.java
@@ -1,8 +1,29 @@
 package src;
+import java.util.*;
 public class Repository {
 
-	private List<Internships> internships;
+	private List<Internship> internships;
 	private List<InternshipApplication> applications;
-	private List<Internships> pendingInternships;
+	private List<Internship> pendingInternships;
+
+	public Internship getInternshipByIndex(int index){
+		for (Internship internship : internships){
+			if (internship.getIndex() == index){
+				return internship;
+			}
+		}
+		throw new IllegalArgumentException("Internship with index " + index + " not found.");
+	}
+
+	public List<Internship> getDisplayInternships(int yearOfStudy, String major){
+		Sort sort =  new Sort();
+		List<Internship> display_list = sort.sortByMajor(internships, major);
+		display_list = sort.sortByYearOfStudy(display_list, yearOfStudy);
+		return display_list;
+	}
+
+	public void addApplication(InternshipApplication application){
+		applications.add(application);
+	}
 
 }
