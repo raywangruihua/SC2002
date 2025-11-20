7569b11fb0a1c85c88db33ef44b9b8c6f12cfbae
cherylphee <cherylphee08@gmail.com>
2025-11-21 02:23:58 +0800

    javadoc student

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -1,10 +1,7 @@
 package model;
 
-import java.util.List;
 import java.util.ArrayList;
-import enums.ApplicationStatus;
-import repository.Repository;
-import ui.StudentPage;
+import java.util.List;
 
 /**
  * Account subtype that has student privieges
@@ -19,6 +16,11 @@ public class Student extends Account {
 
 	/**
 	 * Constructor used when reading student account details from csv file
+	 * @param userID 	the student's unique userID
+	 * @param name 		the student's full name 
+	 * @param password 	the student's account password
+	 * @param year 		the student's year of study (1-4)
+	 * @param major 	the student's major 
 	 */
 	public Student(String userID, String name, String password, int year, String major) {
 		super(userID, name, password);
@@ -27,13 +29,21 @@ public class Student extends Account {
 		this.applications = new ArrayList<>();
 		this.accepted   = null;
 	}
-
+	/**
+	 * returns the student's year of study 
+	 * @return the year of study
+	 */
 	public int 	  getYearOfStudy()  {return yearOfStudy;}
+
+	/**
+	 * returns the student's major
+	 * @return major
+	 */
 	public String getMajor() {return major;}
 	public List<InternshipApplication> getApplications() {return applications;}
 
 	/**
-	 * 
+	 * student accepts an internship 
 	 * @param a
 	 */
 	public void acceptPlacement(InternshipApplication a) {
@@ -41,16 +51,25 @@ public class Student extends Account {
 	}
 
 	/**
-	 * Add application to list of applications 
+	 * add student application to a list of applications 
+	 * @param a 
 	 */
 	public void addApplication(InternshipApplication a) {
 		applications.add(a);
 	}
 
+	/**
+	 * check if student has an internship application
+	 * @return boolean
+	 */
 	public boolean hasAcceptedPlacement(){
 		return (accepted != null);
 	}
 
+	/**
+	 * remove student application from a list of applications 
+	 * @param a
+	 */
 	public void removeApplication(InternshipApplication a){
 		this.applications.remove(a);
 	}
